50e7515b8937d8e664662a192b4c61f2102c23c7
FantasticPan <706390664@qq.com>
2019-01-19 22:52:28 +0800

    算法练习-反转链表

---
+++ src/java/com/pan/NowCoder/Exercise24.java
@@ -0,0 +1,21 @@
+package com.pan.NowCoder;
+
+/**
+ * 反转链表-鲁棒性
+ * Created by FantasticPan on 2019/1/19.
+ */
+public class Exercise24 {
+
+    class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public ListNode ReverseList(ListNode head) {
+
+    }
+}
