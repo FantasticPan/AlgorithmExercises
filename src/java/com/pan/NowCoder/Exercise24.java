32ac468bcef814e99c79702db2e8201dea18cb84
FantasticPan <706390664@qq.com>
2019-01-21 19:11:26 +0800

    算法练习-反转链表

--- src/java/com/pan/NowCoder/Exercise24.java
+++ src/java/com/pan/NowCoder/Exercise24.java
@@ -16,6 +16,47 @@ public class Exercise24 {
     }
 
     public ListNode ReverseList(ListNode head) {
+        //方法1：自己思考的方法，使用栈
+        //if (head == null) {
+        //    return null;
+        //}
+        //ListNode node = head;
+        //ListNode node1 = head;
+        //Stack<Integer> stack = new Stack<>();
+        //while (node != null) {
+        //    stack.push(node.val);
+        //    node = node.next;
+        //}
+        //
+        //node1.val = stack.pop();
+        //ListNode node2 = node1;
+        //node1 = node1.next;
+        //
+        //while (!stack.isEmpty() && node1 != null) {
+        //    node1.val = stack.pop();
+        //    node1 = node1.next;
+        //}
+        //
+        //return node2;
 
+        //方法2：指针实现
+        if (head == null) {
+            return null;
+        }
+
+        ListNode reverseHead = null;
+        ListNode pre = null;
+        ListNode node = head;
+
+        while (node != null) {
+            ListNode next = node.next; //保留下一个节点
+            if (next == null) {
+                reverseHead = node;
+            }
+            node.next = pre; //在这里已经对node.next进行了更改，所以下面要注意
+            pre = node;
+            node = next; //不能写成node = node.next
+        }
+        return reverseHead;
     }
 }
