28416962b5855f7ba1c02f9dd92ea3dc1bd61116
FantasticPan <706390664@qq.com>
2019-01-22 20:12:15 +0800

    算法练习-顺时针打印矩阵

--- src/java/com/pan/NowCoder/Exercise29.java
+++ src/java/com/pan/NowCoder/Exercise29.java
@@ -8,7 +8,44 @@ import java.util.ArrayList;
  */
 public class Exercise29 {
 
-    public ArrayList<Integer> printMatrix(int [][] matrix) {
+    public ArrayList<Integer> printMatrix(int[][] matrix) {
+        if (matrix == null) {
+            return null;
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        int row = matrix.length;
+        int col = matrix[0].length;
 
+        int left = 0;
+        int top = 0;
+        int right = col - 1;
+        int bottom = row - 1;
+
+        while (left <= right && top <= bottom) {
+            for (int i = left; i <= right; i++) {
+                list.add(matrix[top][i]);
+            }
+
+            for (int j = top + 1; j <= bottom; j++) {
+                list.add(matrix[j][right]);
+            }
+
+            if (top != bottom) { //注意这里是!=，不是<=
+                for (int k = right - 1; k >= left; k--) {
+                    list.add(matrix[bottom][k]);
+                }
+            }
+
+            if (left != right) {
+                for (int m = bottom - 1; m > top; m--) {
+                    list.add(matrix[m][left]);
+                }
+            }
+            left++;
+            top++;
+            right--;
+            bottom--;
+        }
+        return list;
     }
 }
