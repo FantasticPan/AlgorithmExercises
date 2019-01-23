1e53f13f97c14086099764ed182eb399f55c7ae4
FantasticPan <706390664@qq.com>
2019-01-23 20:35:58 +0800

    算法练习-从上到下打印二叉树

--- src/java/com/pan/NowCoder/Exercise32.java
+++ src/java/com/pan/NowCoder/Exercise32.java
@@ -1,6 +1,7 @@
 package com.pan.NowCoder;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 /**
  * 从上到下打印二叉树-面试思路-举例让抽象具体化
@@ -19,6 +20,26 @@ public class Exercise32 {
     }
 
     public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+        ArrayList<Integer> treeNode = new ArrayList<>();
+        LinkedList<TreeNode> treeList = new LinkedList<>();
+        if (root == null) {
+            return treeNode;
+        }
+        if (root.left == null && root.right == null) {
+            treeNode.add(root.val);
+            return treeNode;
+        }
 
+        while (root != null) {
+            treeNode.add(root.val);
+            if (root.left != null) {
+                treeList.addLast(root.left);
+            }
+            if (root.right != null) {
+                treeList.addLast(root.right);
+            }
+            root = treeList.pollFirst();
+        }
+        return treeNode;
     }
 }
