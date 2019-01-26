7d840dd386118d86ab007591df366997416808c2
FantasticPan <706390664@qq.com>
2019-01-26 17:46:09 +0800

    算法练习-八大排序-基数排序

---
+++ src/java/com/pan/EightSorts/RadixSort.java
@@ -0,0 +1,35 @@
+package com.pan.EightSorts;
+
+/**
+ * 基数排序
+ * Created by FantasticPan on 2019/1/26.
+ */
+public class RadixSort {
+
+    public void radixSort(int[] array) {
+        if (array == null) {
+            return;
+        }
+
+        //找到最大数
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (max < array[i]) {
+                max = array[i];
+            }
+        }
+
+        int times = 0;
+        while (max > 0) {
+            max /= 10;
+            times++;
+        }
+
+        //位数
+        int count = 1;
+        //保存每一位排序后的结果
+        int k;
+        int lsd;
+        int
+    }
+}
