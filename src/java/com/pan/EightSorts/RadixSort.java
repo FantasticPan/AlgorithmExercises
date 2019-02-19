28bed2b1180a3aaddd82d419458a387831bd35bc
FantasticPan <706390664@qq.com>
2019-02-19 17:07:07 +0800

    算法练习-八大排序-基数排序

--- src/java/com/pan/EightSorts/RadixSort.java
+++ src/java/com/pan/EightSorts/RadixSort.java
@@ -1,35 +1,59 @@
-//package com.pan.EightSorts;
-//
-///**
-// * 基数排序
-// * Created by FantasticPan on 2019/1/26.
-// */
-//public class RadixSort {
-//
-//    public void radixSort(int[] array) {
-//        if (array == null) {
-//            return;
-//        }
-//
-//        //找到最大数
-//        int max = array[0];
-//        for (int i = 1; i < array.length; i++) {
-//            if (max < array[i]) {
-//                max = array[i];
-//            }
-//        }
-//
-//        int times = 0;
-//        while (max > 0) {
-//            max /= 10;
-//            times++;
-//        }
-//
-//        //位数
-//        int count = 1;
-//        //保存每一位排序后的结果
-//        int k;
-//        int lsd;
-//        int
-//    }
-//}
+package com.pan.EightSorts;
+
+/**
+ * 基数排序
+ * Created by FantasticPan on 2019/1/26.
+ */
+public class RadixSort {
+
+    public static void main(String[] args) {
+        //int[] x = {2, 4, 1, 8, 5, 6, 3, 0, 7, 9};
+        //int[] x = {5, 1, 3, 4, 2};
+        //new RadixSort().radixSort(x);
+    }
+
+    public void radixSort(int[] array) {
+        if (array == null) {
+            return;
+        }
+
+        //找到最大数
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (max < array[i]) {
+                max = array[i];
+            }
+        }
+
+        //需要遍历的次数由数组最大值的位数来决定
+        int times = 0;
+        while (max > 0) {
+            max /= 10;
+            ++times;
+        }
+
+        int count = 1;
+        for (int i = 1; i <= times; i++) {
+            int[][] buckets = new int[array.length][10];
+
+            for (int j = 0; j < array.length; j++) {
+                int num = (array[j] / count) % 10;
+                buckets[j][num] = array[j];
+            }
+
+            int k = 0;
+            for (int j = 0; j < 10; j++) {
+                for (int l = 0; l < array.length; l++) {
+                    if (buckets[l][j] != 0) { //java构建int数组默认为0
+                        array[k++] = buckets[l][j];
+                    }
+                }
+            }
+            count *= 10;
+        }
+
+        for (int x : array) {
+            System.out.print(x + " ");
+        }
+    }
+}
---
+++ src/java/com/pan/NowCoder/Exercise38.java
@@ -0,0 +1,14 @@
+//package com.pan.NowCoder;
+//
+//import java.util.ArrayList;
+//
+///**
+// * 分解让复杂问题简单-字符串的排列
+// * Created by FantasticPan on 2019/2/18.
+// */
+//public class Exercise38 {
+//
+//    public ArrayList<String> Permutation(String str) {
+//
+//    }
+//}
